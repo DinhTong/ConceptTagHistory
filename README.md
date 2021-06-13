41a934facd4b0408492636ff8ea0d64d8baae326
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-06-13 15:34:41 -0500

    Add some code to  use dataframes and filter on input

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Life is hard, nothing is perfect. I was given these 2 raw datasets and my task was to generate a full history log as shown. I had to get it done in MySQL 5.6.10 but you don't have to. 
 1. One customer can recieve multiple tags at the same time
 2. We are only interested in INVENTORY tag type
+
+### Create Virtual Environment and install dependencies
+- `c:\>C:\Python39\python -m venv C:\projects\ConceptTagHistory\venv`
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from pathlib import Path
-import datetime
+from tabulate import tabulate
 from prettytable import PrettyTable
 
 def read_tags(csv_file):
@@ -26,3 +26,18 @@ def data_files_to_display(data_files):
 if __name__ == '__main__':
     data_files = get_data_filenames()
     data_files_to_display(data_files)
+    tags_frame = read_tags(next(filter(lambda x: x.name.endswith('Tag.csv'), data_files), None))
+    # row_count = len(tags_frame.index)
+    tags_frame = tags_frame[tags_frame['Type'] == 'INVENTORY']
+    history_frame = read_tags(next(filter(lambda x: x.name.endswith('HistoryLog.csv'), data_files), None))
+    merged = history_frame.merge(tags_frame, how='inner', left_on='HistoryLogID', right_on='TagHistoryLogID')
+    customer_records = pd.DataFrame(merged, columns=['CustomerID', 'Type', 'Tag', 'CreationDateTime'])
+
+    print('Customer ID to lookup?')
+    customer_id = int(input())
+
+    customer_records = customer_records[customer_records["CustomerID"] == customer_id]
+    customer_records['CreationDateTime'] = pd.to_datetime(customer_records['CreationDateTime'])
+    customer_records.sort_values(by=['CreationDateTime'], inplace=True, ascending=False)
+
+    print(tabulate(customer_records, headers='keys', tablefmt='psql'))
